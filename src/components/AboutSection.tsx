0e3c3aafc02da8eb991cad0d103bc06559609d00
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-11-12 07:20:35 -0300

    feat: creacion del componente AboutSection

---
+++ src/components/AboutSection.tsx
@@ -0,0 +1,11 @@
+const AboutSection = () => (
+  <section className="py-12  text-center">
+    <h3 className="text-3xl font-semibold mb-4">Quiénes Somos</h3>
+    <p className="max-w-2xl mx-auto">
+      Somos una empresa dedicada a ofrecer soluciones empresariales innovadoras
+      y personalizadas para cada uno de nuestros clientes.
+    </p>
+  </section>
+);
+
+export default AboutSection;
